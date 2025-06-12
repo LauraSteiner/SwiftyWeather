d425f13c410dde6be4344c2c229746ef4e46c25e
Laura Steiner <steiner.laura@gmail.com>
2025-06-11 20:02:35 -0700

    Initial Commit

---
+++ SwiftyWeatherTests/SwiftyWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherTests.swift
+//  SwiftyWeatherTests
+//
+//  Created by Laura Steiner on 6/11/25.
+//
+
+import Testing
+@testable import SwiftyWeather
+
+struct SwiftyWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
